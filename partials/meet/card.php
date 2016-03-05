cc5acb10d7722d19fdfe098e47dc829ba6759b12
Kimberly Grey <kimandthemachine@gmail.com>
2016-03-05 17:52:02 +0000

    Add 'now happening' indicator to meet cards

--- partials/meet/card.php
+++ partials/meet/card.php
@@ -4,18 +4,25 @@
 	if(has_post_thumbnail()):
 		$meet_image = sb_responsive_image_helper(get_post_thumbnail_id(), "meet-card__image");
 	else: 
-		$meet_image = '<img class="meet-card__map" alt="Map showing the location of ' . $meet_location["name"] . '." srcset="//maps.googleapis.com/maps/api/staticmap?size=320x320&amp;scale=2&amp;maptype=roadmap&amp;markers=color:0xe84545%7C' . $meet_coords . '&amp;&amp;zoom=15&amp;key=AIzaSyBbChsPXFcEPgXCAncMzV5FdaWc2W8_Hjk">';
+		$meet_image = '<img class="meet-card__map" alt="Map showing the location of ' . $meet_location["name"] . '." srcset="//maps.googleapis.com/maps/api/staticmap?size=320x320&amp;scale=2&amp;maptype=roadmap&amp;markers=color:0xe84545%7C' . $meet_coords . '&amp;zoom=15&amp;key=AIzaSyBbChsPXFcEPgXCAncMzV5FdaWc2W8_Hjk">';
 	endif; 
 ?>
 <article class="meet-card meet-grid__item">
 	<a class="meet-card__link" href="<?php the_permalink(); ?>">
+		<?php 
+			if(time() >= get_field("meet_start_time") && time() < get_field("meet_end_time")):
+		?>
+		<div class="meet-card__live">Happening Now!</div>
+		<?php 
+			endif; 
+		?>
 		<div class="meet-card__media">
 			<?php echo $meet_image; ?>
 		</div>
 		<div class="meet-card__body">
 			<h1 class="meet-card__title"><?php the_title(); ?></h1>
-			<div class="meet-card__locality"><?php echo $meet_location["locality"]; ?></div>
 			<ul class="meet-card__meta">
+				<li class="meet-card__meta-item meet-card__meta-item--locality"><?php echo $meet_location["locality"]; ?></li>
 				<li class="meet-card__meta-item"><?php echo sb_meet_dates(get_field("meet_start_time")); ?></li>
 			</ul>
 		</div>
