eeac55847cdd0c82f62c20b6f03d7cc69db5d5a7
Kimberly Grey <kimandthemachine@gmail.com>
2016-04-16 21:37:36 +0100

    Change how locales are output and themes rendered. Closes #32.

--- partials/meet/card.php
+++ partials/meet/card.php
@@ -1,5 +1,6 @@
 <?php 
 	$meet_location = sb_meet_location(get_field("meet_location")); 
+	$meet_theme = !empty($meet_location["locality"]) ? strtolower($meet_location["locality"]) : "severn";
 	$meet_coords = $meet_location["latitude"] . "," . $meet_location["longitude"];
 	if(has_post_thumbnail()):
 		$meet_image = sb_responsive_image_helper(get_post_thumbnail_id(), "meet-card__image");
@@ -7,7 +8,7 @@
 		$meet_image = '<img class="meet-card__map" alt="Map showing the location of ' . $meet_location["name"] . '." srcset="//maps.googleapis.com/maps/api/staticmap?size=320x320&amp;scale=2&amp;maptype=roadmap&amp;markers=color:0xe84545%7C' . $meet_coords . '&amp;zoom=15&amp;key=' . GOOGLE_MAPS_API_KEY . '">';
 	endif; 
 ?>
-<article class="meet-card meet-grid__item" data-theme="<?php echo strtolower($meet_location["locality"]); ?>">
+<article class="meet-card meet-grid__item" data-theme="<?php echo $meet_theme; ?>">
 	<a class="meet-card__link" href="<?php the_permalink(); ?>">
 		<div class="meet-card__media">
 			<?php echo $meet_image; ?>
@@ -15,7 +16,13 @@
 		<div class="meet-card__body">
 			<h1 class="meet-card__title"><?php the_title(); ?></h1>
 			<ul class="meet-card__meta">
+				<?php 
+					if(isset($meet_location["locality"]) && !empty($meet_location["locality"])):
+				?>
 				<li class="meet-card__meta-item meet-card__meta-item--locality"><?php echo $meet_location["locality"]; ?></li>
+				<?php 
+					endif;
+				?>
 				<li class="meet-card__meta-item meet-card__meta-item--date"><?php echo sb_meet_dates(get_field("meet_start_time")); ?></li>
 			</ul>
 		</div>
