aa112b9f1f479fad7250c723e324caca5439e260
Kimberly Grey <kimandthemachine@gmail.com>
2016-04-20 07:56:06 +0100

    Search page and initial form styling

--- functions/navigation.php
+++ functions/navigation.php
@@ -6,7 +6,7 @@
 function sb_search_url_rewrite_rule() {
 	if(is_search() && !empty($_GET['s'])) {
 		wp_redirect(home_url("/search/") . urlencode(get_query_var('s')));
-		exit();
+		exit;
 	} 
 }
 add_action("template_redirect", "sb_search_url_rewrite_rule");
--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <form class="search-form search__form" method="get" action="<?php echo home_url("/"); ?>" role="search" data-search-form>
-	<label for="search-query">
-		Search
-	</label>
-	<input class="search-form__field" type="search" name="s" id="search-query" placeholder="Search&hellip;" value="<?php echo get_search_query() ?>">
-	<button type="submit" class="search-form__submit">Search</button>
+	<label class="search-form__label" for="search-query">Search</label>
+	<input class="search-form__input" type="search" name="s" id="search-query" placeholder="Type to search" value="<?php echo get_search_query() ?>" autofocus>
+	<div class="search-form__buttons">
+		<button type="submit" class="search-form__submit">Search</button>
+	</div>
 </form>
