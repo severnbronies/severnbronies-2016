3a841cf4a6bd2e4f0b04d0e9052b642c7b4ea293
Kimberly Grey <kimandthemachine@gmail.com>
2016-04-25 21:32:31 +0100

    Add cachebusting to compiled assets. Closes #34.

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php 
 
+define("ASSET_VERSION", "1.2.4");
+
 require_once "functions/helpers.php";
 require_once "functions/theme.php";
 require_once "functions/wysiwyg.php";
--- partials/global/html-header.php
+++ partials/global/html-header.php
@@ -19,28 +19,28 @@
 	<meta name="ICBM" content="51.454513, -2.58791">
 
 	<!-- Favicons -->
-	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/apple-touch-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/apple-touch-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/apple-touch-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/apple-touch-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/apple-touch-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/apple-touch-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/apple-touch-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/apple-touch-icon-152x152.png">
-	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/apple-touch-icon-180x180.png">
-	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/favicon-32x32.png" sizes="32x32">
-	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/favicon-194x194.png" sizes="194x194">
-	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/favicon-96x96.png" sizes="96x96">
-	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/android-chrome-192x192.png" sizes="192x192">
-	<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/favicon-16x16.png" sizes="16x16">
-	<link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/manifest.json">
-	<link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/safari-pinned-tab.svg" color="#2b2e4a">
+	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo sb_asset("images/favicon/apple-touch-icon-57x57.png"); ?>">
+	<link rel="apple-touch-icon" sizes="60x60" href="<?php echo sb_asset("images/favicon/apple-touch-icon-60x60.png"); ?>">
+	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo sb_asset("images/favicon/apple-touch-icon-72x72.png"); ?>">
+	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo sb_asset("images/favicon/apple-touch-icon-76x76.png"); ?>">
+	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo sb_asset("images/favicon/apple-touch-icon-114x114.png"); ?>">
+	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo sb_asset("images/favicon/apple-touch-icon-120x120.png"); ?>">
+	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo sb_asset("images/favicon/apple-touch-icon-144x144.png"); ?>">
+	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo sb_asset("images/favicon/apple-touch-icon-152x152.png"); ?>">
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo sb_asset("images/favicon/apple-touch-icon-180x180.png"); ?>">
+	<link rel="icon" type="image/png" href="<?php echo sb_asset("images/favicon/favicon-32x32.png"); ?>" sizes="32x32">
+	<link rel="icon" type="image/png" href="<?php echo sb_asset("images/favicon/favicon-194x194.png"); ?>" sizes="194x194">
+	<link rel="icon" type="image/png" href="<?php echo sb_asset("images/favicon/favicon-96x96.png"); ?>" sizes="96x96">
+	<link rel="icon" type="image/png" href="<?php echo sb_asset("images/favicon/android-chrome-192x192.png"); ?>" sizes="192x192">
+	<link rel="icon" type="image/png" href="<?php echo sb_asset("images/favicon/favicon-16x16.png"); ?>" sizes="16x16">
+	<link rel="manifest" href="<?php echo sb_asset("images/favicon/manifest.json"); ?>">
+	<link rel="mask-icon" href="<?php echo sb_asset("images/favicon/safari-pinned-tab.svg"); ?>" color="#2b2e4a">
 	<meta name="msapplication-TileColor" content="#2b2e4a">
-	<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/dst/images/favicon/mstile-144x144.png">
+	<meta name="msapplication-TileImage" content="<?php echo sb_asset("images/favicon/mstile-144x144.png"); ?>">
 	<meta name="theme-color" content="<?php echo sb_theme_color(); ?>">
 
 	<!-- Stylesheets -->
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/dst/css/stylesheet.css">
+	<link rel="stylesheet" href="<?php echo sb_asset("css/stylesheet.css"); ?>">
 
 	<!-- Preload JavaScript -->
 	<script>
