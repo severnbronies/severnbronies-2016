c8cc26b0ffd50a47571c1e14f56f59f7b071f3bb
Kimberly Grey <kimandthemachine@gmail.com>
2016-08-16 18:39:48 +0100

    Fix up gulp pipes and sass linting

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,23 @@ const gulp = require('gulp');
 const plumber = require('gulp-plumber');
 const fractal = require('./fractal/fractal');
 
-gulp.task('default', ['fractal:watch'], function() {
-	gulp.watch('./src/scss/{,*/}*.scss', ['sass']);
-	gulp.watch('./src/js/{,*/}*.js', ['javascript']);
+gulp.task('default', ['fractal:watch'], () => {
+	gulp.watch('./src/scss/{,*/}*.s+(a|c)ss', ['sass']);
+	gulp.watch('./src/js/+(vendor|preload)/{,*/}*.js', ['js:vendor']);
+	gulp.watch('./src/js/scripts/{,*/}*.js', ['js:custom']);
 	gulp.watch('./src/images/{,*/}*', ['images']);
 	gulp.watch('./src/type/{,*/}*', ['fonts']);
 });
 
-gulp.task('lint', ['sass-lint', 'javascript-lint'])
-gulp.task('production', ['sass', 'javascript', 'images', 'fonts']);
+gulp.task('lint', ['sass:lint', 'js:lint'])
+gulp.task('production', ['sass', 'js:vendor', 'js:custom', 'images', 'fonts']);
 gulp.task('force', ['production', 'fractal:export']);
 
 /*
  * Sass compilation
  */
 
-gulp.task('sass', function() {
+gulp.task('sass', () => {
 	const sass = require('gulp-sass');
 	const autoprefixer = require('gulp-autoprefixer');
 	gulp.src('./src/scss/*.scss')
@@ -34,43 +35,63 @@ gulp.task('sass', function() {
 	.pipe(gulp.dest('./dst/css'));
 });
 
-gulp.task('sass-lint', function() {
+gulp.task('sass:lint', () => {
 	const sassLint = require('gulp-sass-lint');
-	// The sass-lint npm module is broken at the moment, so keeping this disabled.
-	// https://github.com/sasstools/sass-lint/issues/389
-	// return gulp.src('./src/scss/{,*/}*.scss')
-	// .pipe(plumber())
-	// .pipe(sassLint({
-	// 	'config': '.sass-lint.yml'
-	// }))
-	// .pipe(sassLint.format())
-	// .pipe(sassLint.failOnError());
+	return gulp.src('./src/scss/{,*/}*.s+(a|c)ss')
+	.pipe(plumber())
+	.pipe(sassLint())
+	.pipe(sassLint.format())
+	.pipe(sassLint.failOnError());
 });
 
 /**
  * JavaScript compilation
  */
 
-gulp.task('javascript', function() {
+gulp.task('js:vendor', () => {
 	const concat = require('gulp-concat');
 	const uglify = require('gulp-uglify');
 	const merge = require('merge-stream');
-	var folders = ['preload', 'vendor', 'scripts'];
-	var tasks = folders.map(function(folder) {
-		return gulp.src('./src/js/' + folder + '/**/*.js', {
-			base: './src/js/' + folder
+	const folders = ['preload', 'vendor'];
+	const tasks = folders.map(folder => {
+		return gulp.src(`./src/js/${folder}/{,*/}*.js`, {
+			base: `./src/js/${folder}`
 		})
 		.pipe(plumber())
-		.pipe(concat(folder + '.js'))
+		.pipe(concat(`${folder}.js`))
 		.pipe(uglify())
 		.pipe(gulp.dest('./dst/js'))
 	});
 	merge(tasks);
 });
 
-gulp.task('javascript-lint', function() {
+gulp.task('js:custom', () => {
+	const babel = require('gulp-babel');
+	const sourcemaps = require('gulp-sourcemaps');
+	const concat = require('gulp-concat');
+	const uglify = require('gulp-uglify');
+	const merge = require('merge-stream');
+	const folders = ['scripts'];
+	const tasks = folders.map(folder => {
+		return gulp.src(`./src/js/${folder}/{,*/}*.js`, {
+			base: `./src/js/${folder}`
+		})
+		.pipe(plumber())
+		.pipe(sourcemaps.init())
+		.pipe(babel({
+			presets: ['es2015']
+		}))
+		.pipe(concat(`${folder}.js`))
+		.pipe(uglify())
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('./dst/js'))
+	});
+	merge(tasks);
+});
+
+gulp.task('js:lint', () => {
 	const jshint = require('gulp-jshint');
-	return gulp.src(['./src/js/{,*/}*.js', '!./src/js/vendor/{,*/}*.js'])
+	return gulp.src(['./src/js/scripts/{,*/}*.js'])
 	.pipe(jshint())
 	.pipe(jshint.reporter('default'));
 });
@@ -79,20 +100,18 @@ gulp.task('javascript-lint', function() {
  * Image optimisation
  */
 
-gulp.task('images', function() {
+gulp.task('images', () => {
 	const newer = require('gulp-newer');
 	const imagemin = require('gulp-imagemin');
 	gulp.src('./src/images/{,*/}*')
 	.pipe(plumber())
 	.pipe(newer('./dst/images'))
-	.pipe(imagemin(
-		{
-			optimizationLevel: 5, // png
-			progressive: true, // jpg
-			interlaced: true, // gif 
-			multipass: true // svg
-		}
-	))
+	.pipe(imagemin({
+		optimizationLevel: 5, // png
+		progressive: true, // jpg
+		interlaced: true, // gif 
+		multipass: true // svg
+	}))
 	.pipe(gulp.dest('./dst/images'));
 });
 
@@ -100,7 +119,7 @@ gulp.task('images', function() {
  * Typography teleportation
  */
 
-gulp.task('fonts', function() {
+gulp.task('fonts', () => {
 	gulp.src('./src/type/{,*/}*')
 	.pipe(gulp.dest('./dst/type'));
 });
@@ -109,7 +128,7 @@ gulp.task('fonts', function() {
  * Fractal
  */
 
-gulp.task('fractal:watch', function(){
+gulp.task('fractal:watch', () => {
 	const logger = fractal.cli.console;
     const server = fractal.web.server({
         sync: true
@@ -120,7 +139,7 @@ gulp.task('fractal:watch', function(){
     });
 });
 
-gulp.task('fractal:export', function() {
+gulp.task('fractal:export', () => {
 	const logger = fractal.cli.console;
 	const builder = fractal.web.builder();
 	builder.on('progress', (completed, total) => logger.update(`Exported ${completed} of ${total} items`, 'info'));
