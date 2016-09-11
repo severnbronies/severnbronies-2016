93c62d04e677b03ec5f7ac6a4642eb29bbf7f717
Kimberly Grey <kimandthemachine@gmail.com>
2016-09-11 09:49:18 +0100

    Fix social feed images not appearing in Chrome

--- src/js/scripts/app.socialFeed.js
+++ src/js/scripts/app.socialFeed.js
@@ -15,6 +15,11 @@ app.socialFeed = function() {
 			cache: false
 		}).done(function(html) {
 			$container.append(html);
+			// Chrome won't always run the each below in time because the 
+			// browser does the above append in a semi-asynchronous fashion. 
+			// We can resolve this by causing this part of the page to be 
+			// forcibly reflowed by hiding and showing it. 
+			$container.hide().show(0); 
 			$("img:not(.img-ready)").each(function() {
 				app.ui.imageTransition($(this));
 			});
