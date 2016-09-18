0b0e30d361db8153f956c69cb270adfef631d31f
Kimberly Grey <kimandthemachine@gmail.com>
2016-09-18 10:21:23 +0100

    Fix text being excessively split

--- landing-page.php
+++ landing-page.php
@@ -42,27 +42,6 @@
 				</div>
 			</div>
 		</article>
-
-
-		<!-- <article class="page">
-			<div class="page__media">
-				<?php
-					if(has_post_thumbnail()):
-						echo sb_responsive_image_helper(get_post_thumbnail_id(), "page__image");
-					endif; 
-				?>
-			</div>
-			<div class="page__body">
-				<header class="page__header">
-					<h1 class="page__title"><?php the_title(); ?></h1>
-				</header>
-				<div class="content article page__content">
-					<?php the_content(); ?>
-					<p class="page__modified">This page was last modified on
-						<time datetime="<?php the_modified_date("c"); ?>"><?php the_modified_date("jS F Y; g:ia"); ?></time>.</p>
-				</div>
-			</div>
-		</article> -->
 	</main>
 
 <?php
